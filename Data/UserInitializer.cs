1b05604ab831e494f7a11d2040cefa18acd360e0
Student <student@lan.itstep.kiev.ua>
2019-04-08 11:16:52 +0300

    litle changes and add UserInitializer

---
+++ Data/UserInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class UserInitializer : CreateDatabaseIfNotExists<UserContext>
+    {
+        public UserInitializer(UserContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
