f5e5e9a761598e5074a1de6638b9e68ee4b6b926
Student <student@lan.itstep.kiev.ua>
2019-04-08 11:08:52 +0300

    litle changes and add UserContext

---
+++ Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Data
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+        : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+
+    }
+}
